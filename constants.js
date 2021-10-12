d96b4f36038331a59f64ffcbf37345612fa92685
hugoarevalo <harmando@greatwolf.com>
2021-10-12 10:36:16 -0500

    Refactor

---
+++ constants.js
@@ -0,0 +1,8 @@
+exports.BLE_STATES = {
+  UNKNOWN: "unkown",
+  RESETTING: "resetting",
+  UNSUPPORTED: "unsupported",
+  UNAUTHORIZED: "unauthorized",
+  POWEREDOFF: "poweredOff",
+  POWEREDON: "poweredOn",
+};
